b56d4ea1e089f4816f365c6231fe776acc314d89
sOfTheApes <saulrs951@protonmail.com>
2023-07-08 12:35:17 -0700

    make cartpole_dqn.py work

--- samples/cartpole_dqn.py
+++ samples/cartpole_dqn.py
@@ -73,17 +73,16 @@ if __name__ == "__main__":
     state_size = env.observation_space.shape[0]
     action_size = env.action_space.n
     agent = DQNAgent(state_size, action_size)
-    # agent.load("./save/cartpole-dqn.h5")
     done = False
     batch_size = 32
 
     for e in range(EPISODES):
-        state = env.reset()
+        state, info = env.reset()
         state = np.reshape(state, [1, state_size])
         for time in range(500):
             # env.render()
             action = agent.act(state)
-            next_state, reward, done, _ = env.step(action)
+            next_state, reward, done, trunc, _ = env.step(action)
             env.render()
             reward = reward if not done else -10
             next_state = np.reshape(next_state, [1, state_size])
@@ -95,5 +94,3 @@ if __name__ == "__main__":
                 break
             if len(agent.memory) > batch_size:
                 agent.replay(batch_size)
-        # if e % 10 == 0:
-        #     agent.save("./save/cartpole-dqn.h5")
